246809921ec1e0d8d55dacdf48855942c459194a
ashutosh-18k92 <ashutosh.18k92@outlook.com>
2021-03-21 15:06:48 +0530

    flask

--- chuckNorris.py
+++ chuckNorris.py
@@ -1,14 +1,21 @@
 import os
 import requests
-X_RapidAPI_Key = os.getenv("X_RapidAPI_Key")
-url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
+from flask import Flask
+app=Flask(__name__)
 
-headers = {
-    'accept': "application/json",
-    'x-rapidapi-key': X_RapidAPI_Key,
-    'x-rapidapi-host': "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
-    }
+@app.route('/')
+def index:
+    X_RapidAPI_Key = os.getenv("X_RapidAPI_Key")
+    url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
 
-response = requests.request("GET", url, headers=headers)
+    headers = {
+        'accept': "application/json",
+        'x-rapidapi-key': X_RapidAPI_Key,
+        'x-rapidapi-host': "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
+        }
 
-print(response.text)
+    response = requests.request("GET", url, headers=headers)
+    return response.text
+
+if __name__ == "__main__":
+    app.run(debug=true)
